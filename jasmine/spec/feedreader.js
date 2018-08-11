04b22252f6a69de257dadb0381ad6ca326d3458a
Marcy Bursac <msbursac@gmail.com>
2018-08-11 08:42:06 -0500

    test to check for at least 1 entry in the feed container

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,26 +1,24 @@
 /* feedreader.js
  *
- * This is the spec file that Jasmine will read and contains
+ * This is the spec file that Jasmine reads and contains
  * all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
+/* All tests are within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
+
 $(function() {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
+        /* This test makes sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+         * empty.*/
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -70,7 +68,7 @@ $(function() {
 
          /* A test that ensures the menu changes
           * visibility when the menu icon is clicked.*/
-         
+
          it('displays when clicked', function() {
             menuIcon = $('.menu-icon-link');
             menuIcon.click();
@@ -82,8 +80,14 @@ $(function() {
             menuIcon.click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
+    });
+}());
 
     /* TODO: Write a new test suite named "Initial Entries" */
+
+    $(function() {
+
+        describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -92,11 +96,20 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+        beforeEach(function(done) {
+            loadFeed();
+                done();
+            });
+        });
+
+        it('should have at least one entry', function() {
+            expect($('.feed').length > 0).toBe(true);
+        });
+
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });
 }());
